4229c10132bc86d99fa64df59b418b723064792e
isanghyeon <leesanghyun9757@gmail.com>
2025-01-25 17:08:16 +0900

    make list.html in document / and delete useless files

--- src/main/java/com/thc/fallsprbasic/repository/PostimgRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thc.fallsprbasic.repository;
-
-import com.thc.fallsprbasic.domain.Postimg;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostimgRepository extends JpaRepository<Postimg, Long>{
-}
